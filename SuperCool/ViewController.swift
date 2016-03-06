75b639bab22221092d3a4c43a65935de753a394a
zhuoyiz <ginozhang1626@gmail.com>
2016-03-06 10:14:13 -0800

    added files for SuperCool

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var pumpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pump(sender: AnyObject) {
+        coolBg.hidden = false
+        coolLogo.hidden = false
+        pumpButton.hidden = true
+    }
 
 }
 
